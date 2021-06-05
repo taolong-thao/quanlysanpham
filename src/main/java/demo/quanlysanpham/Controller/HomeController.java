2904ec421ff2fa685136e3f04057f107f6af4427
taolong-thao <dfe.angelstyle@gmail.com>
2021-06-05 16:36:27 +0700

    xong san pham

--- src/main/java/demo/quanlysanpham/Controller/HomeController.java
+++ src/main/java/demo/quanlysanpham/Controller/HomeController.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import demo.quanlysanpham.Model.SanPham;
 import demo.quanlysanpham.Services.sanPhamServices;
@@ -24,7 +26,7 @@ public class HomeController {
     @Autowired
     sanPhamServices sanPhamServices;
 
-    @RequestMapping(value = "/longpro", method = RequestMethod.GET)
+    @RequestMapping(value = "/ViewSP", method = RequestMethod.GET)
     public String view(Model model) {
         List<SanPham> list = sanPhamServices.getall();
         model.addAttribute("sanpham", list);
@@ -42,5 +44,19 @@ public class HomeController {
     public String savesp(@ModelAttribute("sanpham") SanPham sanPham) {
         sanPhamServices.save(sanPham);
         return "redirect:/";
+    }
+
+    @GetMapping("/delete")
+    public String del(@RequestParam("masp") String masp) {
+        sanPhamServices.delete(masp);
+        return "redirect:/";
+    }
+
+    @GetMapping("/update/{masp}")
+    public String edit(@PathVariable(value = "masp") String masp, Model model) {
+        //get sp from sevice
+        SanPham sanPham = sanPhamServices.find(masp);
+        model.addAttribute("sanpham", sanPham);
+        return "editSP";
     }
 }
